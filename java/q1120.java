49cb1fca31214c010449d5ef585301085af1af2f
edi279 <jmpark1997@naver.com>
2021-08-09 17:03:16 +0900

    문자열

---
+++ java/q1120.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class q1120 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String A = in.next();
+        String B = in.next(); // 공백으로 구분
+        int min = A.length();
+        for(int i=0;i<=B.length()-A.length();i++){
+            int count = 0;
+            for (int j=0;j<A.length();j++){
+                if (B.charAt(i+j)!=(A.charAt(j))) count++;
+            }           // 낱개 글자 비교할때는 charAt()
+            if (min>count) min = count;
+        }
+        System.out.println(min);
+    }
+}
