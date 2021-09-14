f08dd2f5d7de405b775d442a9fb0d20affb09fe2
edi279 <jmpark1997@naver.com>
2021-09-14 15:23:59 +0900

    돌 게임 2

---
+++ java/q9656.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class q9656{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N%2==0) System.out.println("SK"); // 1개 또는 3개씩 가져가므로
+        else System.out.println("CY"); // 수량의 홀짝 여부에 따라 승자 결정
+    }
+}
