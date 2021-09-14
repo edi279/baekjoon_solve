f71b26bf6381ff2ef87c591d69f402ee6f18b6f0
edi279 <62528633+edi279@users.noreply.github.com>
2021-09-14 15:27:12 +0900

    9656 돌 게임 2

--- java/q9656.java
+++ java/q9656.java
@@ -6,6 +6,6 @@ public class q9656{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         if (N%2==0) System.out.println("SK"); // 1개 또는 3개씩 가져가므로
-        else System.out.println("CY"); // 수량의 홀짝 여부에 따라 승자 결정
+        else System.out.println("CY"); // 수량의 홀짝 여부에 따라 승자 결정 
     }
 }
